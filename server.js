6fa0e3146cd56dfd90290dc092250187e4b727d6
Elias Fikadu <eliasfikadu991@gmail.com>
2026-09-21 13:35:55 +0100

    Fix Render port binding

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const Order = require("./models/Order");
 const User = require("./models/User");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -297,5 +297,5 @@ app.post("/api/login", async (req, res) => {
 // ===============================
 
 app.listen(PORT, () => {
-  console.log('Server running on http://localhost:${PORT}');
+  console.log(`Server running on http://localhost:${PORT}`);
 });
